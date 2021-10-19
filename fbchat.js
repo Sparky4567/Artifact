1336cf0514ba79ee73f9bc2fc7342048e69ca79e
Andrius Pratusis <andrius.pratusis1993@gmail.com>
2021-10-19 20:33:39 +0300

    Update fbchat.js
    Adding timeout to check the chat

--- fbchat.js
+++ fbchat.js
@@ -1,25 +1,30 @@
 if (config.messenger === true) {
 	if (document.getElementById("fb-customer-chat")) {
-		var chatbox = document.getElementById("fb-customer-chat");
-		if (chatbox.length) {
-			chatbox.setAttribute("page_id", config.fbpageId);
-			chatbox.setAttribute("attribution", "biz_inbox");
-
-			window.fbAsyncInit = function () {
-				FB.init({
-					xfbml: true,
-					version: "v12.0",
-				});
-			};
-			(function (d, s, id) {
-				var js,
-					fjs = d.getElementsByTagName(s)[0];
-				if (d.getElementById(id)) return;
-				js = d.createElement(s);
-				js.id = id;
-				js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-				fjs.parentNode.insertBefore(js, fjs);
-			})(document, "script", "facebook-jssdk");
-		}
+		setTimeout(() => {
+			if(document.getElementById("fb-customer-chat"){
+				var chatbox = document.getElementById("fb-customer-chat");
+				if (chatbox.length) {
+					chatbox.setAttribute("page_id", config.fbpageId);
+					chatbox.setAttribute("attribution", "biz_inbox");
+		
+					window.fbAsyncInit = function () {
+						FB.init({
+							xfbml: true,
+							version: "v12.0",
+						});
+					};
+					(function (d, s, id) {
+						var js,
+							fjs = d.getElementsByTagName(s)[0];
+						if (d.getElementById(id)) return;
+						js = d.createElement(s);
+						js.id = id;
+						js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+						fjs.parentNode.insertBefore(js, fjs);
+					})(document, "script", "facebook-jssdk");
+				}
+			}
+		}, 1000);
+	
 	}
 }
