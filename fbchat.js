f858db95aa4876a2662f03d4810aed8187d82485
Andrius Pratusis <andrius.pratusis1993@gmail.com>
2021-10-19 18:25:55 +0300

    Update fbchat.js
    One more fix

--- fbchat.js
+++ fbchat.js
@@ -1,23 +1,25 @@
 if (config.messenger === true) {
-	var chatbox = document.getElementById("fb-customer-chat");
-	if (chatbox.length) {
-		chatbox.setAttribute("page_id", config.fbpageId);
-		chatbox.setAttribute("attribution", "biz_inbox");
+	if (document.getElementById("fb-customer-chat")) {
+		var chatbox = document.getElementById("fb-customer-chat");
+		if (chatbox.length) {
+			chatbox.setAttribute("page_id", config.fbpageId);
+			chatbox.setAttribute("attribution", "biz_inbox");
 
-		window.fbAsyncInit = function () {
-			FB.init({
-				xfbml: true,
-				version: "v12.0",
-			});
-		};
-		(function (d, s, id) {
-			var js,
-				fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) return;
-			js = d.createElement(s);
-			js.id = id;
-			js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-			fjs.parentNode.insertBefore(js, fjs);
-		})(document, "script", "facebook-jssdk");
+			window.fbAsyncInit = function () {
+				FB.init({
+					xfbml: true,
+					version: "v12.0",
+				});
+			};
+			(function (d, s, id) {
+				var js,
+					fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) return;
+				js = d.createElement(s);
+				js.id = id;
+				js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+				fjs.parentNode.insertBefore(js, fjs);
+			})(document, "script", "facebook-jssdk");
+		}
 	}
 }
