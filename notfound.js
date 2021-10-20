6e6df0880d3c78c6647940840f39efc37e64ab70
Andrius Pratusis <andrius.pratusis1993@gmail.com>
2021-10-20 23:01:26 +0300

    This one should not be here

--- notfound.js
+++ notfound.js
@@ -1,30 +1,29 @@
 class NotFound extends React.Component {
-  constructor(props) {
-    super(props);
-    this.style = {
-      backgroundColor: "#212121",
-      color: "white",
-    };
-    this.heightStyle = {};
-  }
+	constructor(props) {
+		super(props);
+		this.style = {
+			backgroundColor: "#212121",
+			color: "white",
+		};
+	}
 
-  render() {
-    return (
-      <div
-        className="container-fluid fill-remaining"
-        style={this.containerStyle}
-      >
-        <div
-          className="row text-center align-items-center fill-remaining justify-content-center flex-grow-1"
-          style={this.style}
-        >
-          <div className="col-lg-8 col-md-8 col-sm-12 col-xs-12 mx-auto text-center">
-            <h1>{config.notfoundError}</h1>
-            <h4>{config.notfoundTitle}</h4>
-            <h4>{config.notfoundText}</h4>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div
+				className="container-fluid fill-remaining"
+				style={this.containerStyle}
+			>
+				<div
+					className="row text-center align-items-center fill-remaining justify-content-center flex-grow-1"
+					style={this.style}
+				>
+					<div className="col-lg-8 col-md-8 col-sm-12 col-xs-12 mx-auto text-center">
+						<h1>{config.notfoundError}</h1>
+						<h4>{config.notfoundTitle}</h4>
+						<h4>{config.notfoundText}</h4>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
