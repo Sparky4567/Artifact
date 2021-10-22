2ff2e70595cfe9dacaa9c4d09a0894573740dcdd
Andrius Pratusis <andrius.pratusis1993@gmail.com>
2021-10-22 11:13:47 +0300

    Fb chat

--- fbchat.js
+++ fbchat.js
@@ -7,31 +7,32 @@ class FbChat extends React.Component {
 	}
 
 	componentDidMount() {
-		if (config.messenger.enabled === true && config.messenger.fbpageId !== "") {
-			if (this.state.flag === null) {
-				var chatbox = document.getElementById("fb-customer-chat");
-				chatbox.setAttribute("page_id", config.messenger.fbpageId);
-				chatbox.setAttribute("attribution", "biz_inbox");
+		if (
+			config.messenger.enabled === true &&
+			config.messenger.fbpageId !== "" &&
+			this.state.flag === null
+		) {
+			var chatbox = document.getElementById("fb-customer-chat");
+			chatbox.setAttribute("page_id", config.messenger.fbpageId);
+			chatbox.setAttribute("attribution", "biz_inbox");
 
-				window.fbAsyncInit = function () {
-					FB.init({
-						xfbml: true,
-						version: "v12.0",
-					});
-				};
+			window.fbAsyncInit = function () {
+				FB.init({
+					xfbml: true,
+					version: "v12.0",
+				});
+			};
 
-				(function (d, s, id) {
-					var js,
-						fjs = d.getElementsByTagName(s)[0];
-					if (d.getElementById(id)) return;
-					js = d.createElement(s);
-					js.id = id;
-					js.src =
-						"https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-					fjs.parentNode.insertBefore(js, fjs);
-				})(document, "script", "facebook-jssdk");
-				this.setState({ flag: true });
-			}
+			(function (d, s, id) {
+				var js,
+					fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) return;
+				js = d.createElement(s);
+				js.id = id;
+				js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+				fjs.parentNode.insertBefore(js, fjs);
+			})(document, "script", "facebook-jssdk");
+			this.setState({ flag: true });
 		}
 	}
 
