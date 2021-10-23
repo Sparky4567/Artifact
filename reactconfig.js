b407aa36ca0140de545bbcbf16b21f3abc53b73b
Andrius Pratusis <andrius.pratusis1993@gmail.com>
2021-10-23 12:35:51 +0300

    Examplary config

---
+++ reactconfig.js
@@ -0,0 +1,102 @@
+//artefaktaseu
+
+let config = {
+	//Keys
+
+	//Blogger v3 api key
+	key: "",
+
+	//Blog id
+	blogid: "1016451689931209365",
+
+	//Sharethis key
+	shareKey: "605758e051f70600114ff5d1",
+
+	//Get code from discus
+	//Selecting add code snippet
+	//Copy the part
+	//From https to js
+	discusScript: "https://artefaktas-eu.disqus.com/embed.js",
+
+	//The image that will be displayed
+	//If no image exists on a blogpost
+	graphImageUrl:
+		"https://blogger.googleusercontent.com/img/a/AVvXsEjqlLRsqmwbNvXcXqBqN-4fMp-NnbArtSw7-iYCGLTobUDSCFrntl_ypCqUHeRi-Z0bc4tbDyTpKZyw8cR0kMVblKOGHjiHqIgqF9UmIKa8rnNlaiCSiNl7t5K9sUtqIxomiMZmn_FSy75Y-gJfJJrJfd8VYcLYFkCm4GSo1nEGx4X7nbxjMFdr5wMIvQ=s16000",
+
+	//About me post url
+	aboutMepostUrl: "https://www.artefaktas.eu/2021/07/apie-mane.html",
+
+	//Contact post url
+	contactsPostUrl: "https://www.artefaktas.eu/2019/10/contacts.html",
+
+	//Posts
+
+	//Max amount to display
+	maxCount: 9,
+
+	//Next button text
+	nextTitle: "Sekantis",
+	//Previous button text
+	previousTitle: "Ankstesnis",
+	//Back to main button text
+	backtomainTitile: "Atgal į pagrindinį",
+	//Read more button text
+	readmoreTitle: "Skaityti daugiau",
+	//Published by text
+	publishedbyTitle: "Paskelbė: ",
+	//Published on text
+	publishedonText: "Paskelbta: ",
+
+	//Carousel enable
+	carousel: true,
+
+	//Menubar text
+
+	//Home page title
+	mainpageTitle: "Pagrindinis",
+	//Contact page title
+	contactpageTitle: "Kontaktai",
+	//About me title
+	aboutmepageTitle: "Apie",
+
+	//Load more
+	//Button text
+	loadmoreTitle: "Daugiau įrašų",
+
+	//SearchPlaceHolderText
+	searchplaceholderText: "Aš ieškau...",
+
+	//Adsense support id after (ca-pub-)
+	//You fill find by clicking
+	//Get code
+	adsense: {
+		enabled: true,
+		adsenseId: "7918415788327185",
+		adSlot: "1049533610",
+	},
+
+	//FB Messenger support
+	//set to true to activate
+	// whitelist your domain on
+	//fb page messenger integration settings
+	messenger: {
+		enabled: true,
+		fbpageId: "816365865431100",
+	},
+	//404
+	notfoundError: "404",
+	notfoundText: " . . . Ar . . . Mano šuo vėl nugriaužė laidus ???",
+
+	//Google Analytics support
+	//Set to true to enable, do not forget to add Analytics id
+	gA: {
+		enabled: true,
+		gtagId: "G-ZP0ECFWD9R",
+	},
+
+	//OneSignalSupport
+	oneSignal: {
+		enabled: true,
+		onesignalId: "c6856f54-ad54-4e14-879f-b980eb8d86c3",
+	},
+};
