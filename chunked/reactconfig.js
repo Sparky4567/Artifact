f9fe6ab1d29ec797a44f5a814830c547fb04fb48
Andrius Pratusis <andrius.pratusis1993@gmail.com>
2021-10-24 19:55:17 +0300

    You did not... I compiled it into chunks

---
+++ chunked/reactconfig.js
@@ -0,0 +1,64 @@
+//artefaktase
+
+let config = {
+	aboutMepostUrl: "https://artefaktase.blogspot.com/2021/10/about-me.html",
+	aboutmepageTitle: "About me",
+	adsenseId: "",
+	backtomainTitile: "Back to main",
+	blogid: "6298745841393040373",
+	carousel: true,
+	contactpageTitle: "Contacts",
+	contactsPostUrl:
+		"https://artefaktase.blogspot.com/2021/10/contact-information.html",
+	discusScript: "https://artefaktase.disqus.com/embed.js",
+	graphImageUrl: "",
+
+	key: "",
+	loadmoreTitle: "Load more",
+	mainpageTitle: "Home",
+	maxCount: 9,
+	messenger: false,
+	nextTitle: "Next",
+	previousTitle: "Previous",
+	publishedbyTitle: "Published by: ",
+	publishedonText: "Published on: ",
+	readmoreTitle: "Read more",
+	searchplaceholderText: "I am looking for...",
+	shareKey: "61672af6e35b180013fb28a1",
+	//Adsense support id after (ca-pub-)
+	//You fill find by clicking
+	//Get code
+	adsense: {
+		enabled: false,
+		adsenseId: "",
+		adSlot: "",
+	},
+	//FB Messenger support
+	//set to true to activate
+	// whitelist your domain on
+	//fb page messenger integration settings
+	//FB Messenger support
+	//set to true to activate
+	// whitelist your domain on
+	//fb page messenger integration settings
+	messenger: {
+		enabled: false,
+		fbpageId: "",
+	},
+	gA: {
+		enabled: false,
+		gtagId: "",
+	},
+
+	//OneSignalSupport
+	oneSignal: {
+		enabled: false,
+		onesignalId: "",
+	},
+
+	//404
+	notfoundError: "404",
+	notfoundText: " . . . Wait . . . Did my dog bite the wires again ???",
+};
+
+window.config = config;
